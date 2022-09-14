44ad364ab1d7943ec3c29607ffcb74c83c61bab7
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 18:50:41 -0500

    Add new Post methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def self.update_posts_counter(user)
+    user.posts_counter = Post.where(author: user).count
+  end
+
+  def five_most_recent_comments(post = self)
+    post.comments.order(updated_at: :desc).first(5)
+  end
 end
