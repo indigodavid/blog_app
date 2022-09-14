c8e74af18af6f61562bad7646fc4158b9d336df1
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 18:51:02 -0500

    Add new Comment methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def self.update_comments_counter(post)
+    post.comments_counter = Comment.where(post: post).count
+  end
 end
