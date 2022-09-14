7a783da4361924de854c32f5ac850dc0f86e7d7f
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 18:50:27 -0500

    Add new User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, inverse_of: 'author'
   has_many :likes, inverse_of: 'author'
   has_many :comments, inverse_of: 'author'
+
+  def last_three_posts
+    Post.where(author: self).order(updated_at: :desc).first(3)
+  end
 end
