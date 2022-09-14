18a43033edd487f147a034e0a36ba35086260d52
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 18:51:20 -0500

    Add new Like methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def self.update_likes_counter(post)
+    post.likes_counter = Like.where(post: post).count
+  end
 end
