4260c1f2cf050fe895ac74502258fc7b6746a01f
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 21:23:14 -0500

    Add foreign_key and refactor update_posts_counter on Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :likes
   has_many :comments
 
-  def self.update_posts_counter(user)
-    user.posts_counter = Post.where(author: user).count
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 
-  def five_most_recent_comments(post = self)
+  def five_most_recent_comments
     post.comments.order(updated_at: :desc).first(5)
   end
 end
