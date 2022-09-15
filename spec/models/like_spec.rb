bfc1e36dee6b6093b9668f5c886a05dbe2a8f991
David Vera Castillo <indigodavid@gmail.com>
2022-09-15 13:33:24 -0500

    Create Like tests. Tests passed

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  new_post = Post.create(title: 'My first post', author: User.create(name: 'David'))
+  other_user = User.create(name: 'Elson')
+  subject { Like.create(author: other_user, post: new_post)}
+
+  it 'UpdatesLikesCounter increments post likes_counter' do
+    expect(subject.post.likes_counter).to be 1
+    Like.create(author: subject.author, post: subject.post)
+    expect(subject.post.likes_counter).to be 2
+  end
 end
