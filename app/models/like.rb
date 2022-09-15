7728d5f356bc9a0e7d51a5e1b6faac9be1c90c8c
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 21:27:00 -0500

    Add foreign_key and refactor update_likes_counter on Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
-  def self.update_likes_counter(post)
-    post.likes_counter = Like.where(post:).count
+  after_save :update_likes_counter
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
