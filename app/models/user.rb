dd6a932f9d227f7be0fe8feb9808dc76fad9911c
David Vera Castillo <indigodavid@gmail.com>
2022-09-14 21:19:10 -0500

    Refactor bidirectional associations using foreign_key on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, inverse_of: 'author'
-  has_many :likes, inverse_of: 'author'
-  has_many :comments, inverse_of: 'author'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 
   def last_three_posts
     Post.where(author: self).order(updated_at: :desc).first(3)
