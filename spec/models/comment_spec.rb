7eb18f29101242ccced24e217a37a3c80bf0eb25
David Vera Castillo <indigodavid@gmail.com>
2022-09-15 13:06:36 -0500

    Create Comment tests. Tests passed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  new_post = Post.create(title: 'My first post', author: User.create(name: 'David'))
+  other_user = User.create(name: 'Elson')
+  subject { Comment.create(text: 'Hi comment', author: other_user, post: new_post)}
+
+  it 'UpdatesCommentsCounter increments post comment_counter' do
+    expect(subject.post.comments_counter).to be 1
+    Comment.create(text: 'Hi comment', author: subject.author, post: subject.post)
+    expect(subject.post.comments_counter).to be 2
+  end
 end
