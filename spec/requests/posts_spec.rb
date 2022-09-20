b58e9082450ace38ba199e149a1c5ae33b7d4386
David Vera Castillo <indigodavid@gmail.com>
2022-09-20 10:57:21 -0500

    Add tests for Post controller index action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+  before(:each) do
+    @user = User.create(name: 'David')
+    @id = @user.id
+  end
+
+  describe 'GET /users/:id/posts/' do
+    before(:example) { get "/users/#{@id}/posts/" }
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include('<h1>Posts</h1>')
     end
   end
 
