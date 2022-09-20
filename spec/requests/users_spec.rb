7f3901ff86b3acf13f96ae9a742fef456ff69c26
David Vera Castillo <indigodavid@gmail.com>
2022-09-20 10:56:58 -0500

    Add tests for User controller show action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /' do
-    before(:example) { get '/'}
+    before(:example) { get '/' }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
@@ -17,10 +17,22 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET users/:id/' do
+    before(:example) do
+      user = User.create(name: 'David')
+      get "/users/#{user.id}/"
+    end
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include('<h1>User#Name placeholder</h1>')
     end
   end
 end
