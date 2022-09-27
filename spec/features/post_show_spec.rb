9beee412df4863ded3a2698ab15e3c0a0a30a4a7
David Vera Castillo <indigodavid@gmail.com>
2022-09-27 10:25:33 -0500

    Fix test describe names

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :system do
-  describe 'index page' do
+  describe 'show page' do
     before(:each) do
       @new_photo = 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
       @first_user = User.create(name: 'Tom', photo: @new_photo, bio: 'Teacher from Mexico.')
