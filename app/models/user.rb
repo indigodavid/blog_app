2cbe75fa73f984d8fab98608b5e5fe2af0dd4868
David Vera Castillo <indigodavid@gmail.com>
2022-09-28 17:33:09 -0500

    Add admin? authorization and dependencies in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true
 
   def last_three_posts
     Post.includes([:comments]).where(author: self).order(created_at: :desc).first(3)
+  end
+
+  def admin?
+    role == 'admin'
   end
 end
