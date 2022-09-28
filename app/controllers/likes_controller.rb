b83b383ca981d3dcb8abb60919bcb63dedd77256
David Vera Castillo <indigodavid@gmail.com>
2022-09-28 17:53:01 -0500

    Fix likes controller redirect

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,11 +8,10 @@ class LikesController < ApplicationController
 
     if @like.save
       flash[:success] = 'Like saved successfully'
-      redirect_to user_post_path(@post.author, @post)
     else
       flash.now[:error] = 'Error: Like could not be saved'
-      render :new, status: 422
     end
+    redirect_to user_post_path(@post.author, @post)
   end
 
   private
