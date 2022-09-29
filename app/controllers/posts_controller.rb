c7545ed9a7a15630a4963750d785c1dafe2b1564
David Vera Castillo <indigodavid@gmail.com>
2022-09-28 19:54:01 -0500

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-  
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes([:comments]).order(created_at: :desc)
