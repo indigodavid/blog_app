e03755d327d778083ea3b66ac3d78e3f28efb3d8
David Vera Castillo <indigodavid@gmail.com>
2022-10-03 09:01:54 -0500

    Add login method for API on AuthenticationController

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::AuthenticationController < ApplicationController
+  include JsonWebToken
+
+  skip_before_action :verify_authenticity_token
 
   def login
+    @user = User.find_by_email(params[:email])
+    if @user.valid_password? params[:password]
+      token = jwt_encode(user_id: @user.id)
+      render json: { token: }, status: :ok
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 end
